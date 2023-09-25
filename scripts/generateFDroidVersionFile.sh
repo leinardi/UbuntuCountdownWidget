e67328ae52187808b0fc497411ec3df2d3406a50
Roberto Leinardi <leinardi@gmail.com>
2023-09-25 10:11:14 +0200

    Update dependencies

--- scripts/generateFDroidVersionFile.sh
+++ scripts/generateFDroidVersionFile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 cat app/build/outputs/app_versioning/release/version_name.txt <(echo -n "+") app/build/outputs/app_versioning/release/version_code.txt > version.txt
+cat version_name.txt
